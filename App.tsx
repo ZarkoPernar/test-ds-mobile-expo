794de613e02a3cb39b71fca1b9b6310c8642137f
Zarko Pernar <zarko.pernar@gmail.com>
2021-02-20 00:25:29 +0000

    init

--- App.tsx
+++ App.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {SafeAreaView, ScrollView, Text, View} from "react-native";
+import {boolean, string} from "yup";
+import {
+	DesignSystemProvider,
+	Box,
+	Stack,
+	FormGroup,
+	TextInput,
+	Form,
+	CheckBox,
+} from "./ds";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<DesignSystemProvider>
+			<SafeAreaView style={{flex: 1}}>
+				<ScrollView style={{flex: 1}}>
+					<Box padding="medium">
+						<Form
+							initialValues={{checkbox: false}}
+							validationSchema={{
+								email: string().required().email(),
+								password: string().required().min(8),
+								checkbox: boolean().oneOf(
+									[true],
+									// "This checkbox must be checked",
+								),
+							}}
+						>
+							<Stack space="medium">
+								<FormGroup
+									label="Email"
+									validation={string().required().email()}
+								>
+									<TextInput
+										name="email"
+										placeholder="joe@unibuddy.com"
+									/>
+								</FormGroup>
+								<FormGroup label="Password">
+									<TextInput
+										type="password"
+										name="password"
+									/>
+								</FormGroup>
+								<CheckBox name="checkbox">
+									I agree to receiving marketing
+									communications from the Institution via the
+									Unibuddy platform and via email. The
+									marketing communications will contain
+									further information about the institution,
+									its courses and other information considered
+									to be of legitimate interest to you.
+								</CheckBox>
+							</Stack>
+						</Form>
+					</Box>
+				</ScrollView>
+			</SafeAreaView>
+		</DesignSystemProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
